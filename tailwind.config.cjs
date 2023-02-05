826009458c393cffc283298be0f7a88aff59aa10
César O. Araújo <oaraujocesar@gmail.com>
2023-02-05 14:59:33 -0300

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: "#ebebeb",
+        darkGrey: "#606060",
+        darkestGrey: "#444444",
+        white: "#FFF",
+      },
+      borderRadius: {
+        default: "9px",
+      },
+    },
+  },
+  plugins: [],
+};
