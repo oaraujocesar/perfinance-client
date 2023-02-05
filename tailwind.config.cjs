b02a121fed81242c957df9bf421dce54b0ef606a
César O. Araújo <oaraujocesar@gmail.com>
2023-02-05 17:58:49 -0300

    add sidebar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        layout: '80px 1fr'
+      },
       colors: {
         grey: "#ebebeb",
-        darkGrey: "#606060",
-        darkestGrey: "#444444",
+        darkgrey: "#606060",
+        darkestgrey: "#444444",
         white: "#FFF",
       },
       borderRadius: {
