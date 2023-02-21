aa7e0203a476e20a8bd035a503ce5acc95a54432
César O. Araújo <oaraujocesar@gmail.com>
2023-02-20 21:41:52 -0300

    add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: [, "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#51459e",
+      secondary: "#f9896b",
+      bgblue: "#f5f9fd",
+      darkText: "#5b5b5b",
+    },
     extend: {},
   },
   plugins: [],
