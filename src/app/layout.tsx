c5e13a5f6dd96f81627bc55b0413fb3b05a1a8fd
César O. Araújo <oaraujocesar@gmail.com>
2023-02-20 22:37:01 -0300

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,36 @@
 "use client";
 
+import localFont from "@next/font/local";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+
 import "@globals/styles/reset.css";
 import "@globals/styles/globals.css";
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { theme } from "@globals/styles/theme";
+
+const googleSans = localFont({
+  src: [
+    {
+      path: "../globals/fonts/GoogleSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../globals/fonts/GoogleSans-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../globals/fonts/GoogleSans-Medium.woff2",
+      weight: "600",
+      style: "semibold",
+    },
+    {
+      path: "../globals/fonts/GoogleSans-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +41,7 @@ export default function RootLayout({
     <ThemeProvider theme={theme}>
       <html lang="en">
         <head />
-        <body>{children}</body>
+        <body className={googleSans.className}>{children}</body>
       </html>
     </ThemeProvider>
   );
