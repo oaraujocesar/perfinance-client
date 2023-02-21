e3084d7334caf94ee91ecf9c82da7b207f665931
César O. Araújo <oaraujocesar@gmail.com>
2023-02-20 21:25:47 -0300

    initial of initial of initial commit lol

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  sassOptions: {
-    includePaths: [
-      path.join(__dirname, "app", "**"),
-    ],
-  },
   experimental: {
     appDir: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "@globals/styles/reset.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
