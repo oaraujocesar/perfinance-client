5fb1bea8fd102f2e9aa6490f51ed3b14054c00aa
César O. Araújo <oaraujocesar@gmail.com>
2023-02-27 21:55:14 -0300

    validation messages in portuguese

--- src/app/login/login.validation.ts
+++ src/app/login/login.validation.ts
@@ -2,11 +2,11 @@ import * as yup from "yup";
 
 export const schema = yup
   .object({
-    email: yup.string().required().email(),
+    email: yup.string().required('O e-mail é obrigatório').email('Endereço de e-mail inválido'),
     password: yup
       .string()
-      .required("Password is required")
-      .min(8, "Password must be min 8 characters"),
+      .required("A senha é obrigatória")
+      .min(8, "A senha deve conter no mínimo 8 caracteres"),
     //   .test(
     //     "regex",
     //     "Password must be min 8 characters, and have 1 Special Character, 1 Uppercase, 1 Number and 1 Lowercase",
