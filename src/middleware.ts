b361f1cccd36e357eb87e84c9ad69d6f12cb2b10
César O. Araújo <oaraujocesar@gmail.com>
2023-02-27 22:01:09 -0300

    rename login to signin

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const isOnPublicRoute =
-    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/signin") ||
     request.nextUrl.pathname.startsWith("/signup");
 
   if (isOnPublicRoute) {
     return NextResponse.next();
   } else {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 }
 
